cc6b04990066976a4918928adb840d9e436aa824
Maxim <bmw_077@mail.ru>
2021-10-08 11:48:41 +0300

    modal window

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="app">
-    <post-form @create="createPost" />
+    <h1>Старница с постами</h1>
+    <my-button @click="showDialog" style="margin: 15px 0px"
+      >Создать пост</my-button
+    >
+    <my-dialog v-model:show="dialogVisible">
+      <post-form @create="createPost" />
+    </my-dialog>
     <post-list :posts="posts" @remove="removePost" />
   </div>
 </template>
@@ -9,6 +15,7 @@
 // импорт компонентов
 import PostForm from "@/components/PostForm";
 import PostList from "@/components/PostList";
+import MyDialog from "./components/UI/MyDialog.vue";
 
 //  в эту секцию по дефолту мы должны экспортировать объект. По сути этот объект и будет являться компонентом
 export default {
@@ -16,6 +23,7 @@ export default {
   components: {
     PostForm,
     PostList,
+    MyDialog,
   },
 
   data() {
@@ -27,15 +35,20 @@ export default {
         { id: 3, title: "Js_3", body: "Описание_3" },
         { id: 4, title: "Js_4", body: "Описание_4" },
       ],
+      dialogVisible: false,
     };
   },
   //   функции объявляются в поле methods у компонента
   methods: {
     createPost(post) {
       this.posts.push(post);
+      this.dialogVisible = false;
     },
     removePost(post) {
       this.posts = this.posts.filter((p) => p.id !== post.id);
+    },
+    showDialog() {
+      this.dialogVisible = true;
     },
   },
 };
